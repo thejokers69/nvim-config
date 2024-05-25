995db3969597365dfa674b7c2cc4cceaff534e43
thejokers69 <thejokkers69@gmail.com>
2024-05-25 10:10:17 +0100

    Removed nvim submodule and added as a regular directory

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,14 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Set environment variables
+set -x PATH /usr/local/bin $PATH
+
+# Set the default editor to nvim
+set -x EDITOR nvim
+
+# Alias definitions
+alias ll 'ls -la'
+alias gs 'git status'
+
+# Source any additional configurations if they exist
+if test -f "$HOME/Library/Application Support/amazon-q/shell/bash_profile.pre.fish"
+    source "$HOME/Library/Application Support/amazon-q/shell/bash_profile.pre.fish"
 end
