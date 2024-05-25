ca0352ea0b08bef48719885d74328f474bb6408d
thejokers69 <thejokkers69@gmail.com>
2024-05-25 23:15:27 +0100

    Created plugins inside lua and created init.lua

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+	-- Solarized color scheme plugin
+	{
+		"shaunsingh/solarized.nvim",
+		config = function()
+			vim.cmd("colorscheme solarized")
+		end,
+	},
+	-- Add other plugins here...
+}
