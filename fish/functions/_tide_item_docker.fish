e39a8e851ef7bac8863774b8c03886fa660795a3
thejokers69 <thejokkers69@gmail.com>
2025-03-17 22:19:42 +0000

    Add new config and update variables
    - Created a backup file for post initialization.
    - Updated the nvm configuration to ensure proper variable setting.
    - Removed an unused plugin from the list.
    - Adjusted universal variables for better compatibility and performance.

--- fish/functions/_tide_item_docker.fish
+++ fish/functions/_tide_item_docker.fish
@@ -1,13 +1,5 @@
 function _tide_item_docker
-    # Check if Docker daemon socket exists
-    if test -e /Users/thejoker/.docker/run/docker.sock
-        # Check if Docker daemon is running
-        if docker info > /dev/null 2>&1
-            # If daemon is running, fetch and display the current context
-            docker context inspect --format '{{.Name}}' | read -l context
-            contains -- "$context" $tide_docker_default_contexts ||
-                _tide_print_item docker $tide_docker_icon' ' $context
-        end
-    end
+    docker context inspect --format '{{.Name}}' | read -l context
+    contains -- "$context" $tide_docker_default_contexts ||
+        _tide_print_item docker $tide_docker_icon' ' $context
 end
-
