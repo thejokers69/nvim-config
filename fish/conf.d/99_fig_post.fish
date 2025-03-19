db3f408c156385e8b180f25a858fba0cdec73c00
thejokers69ML <thejokkers69@gmail.com>
2025-03-19 00:19:23 +0000

    MacBook Pro: Update Fish config with Tide and brew coloring

--- fish/conf.d/99_fig_post.fish
+++ fish/conf.d/99_fig_post.fish
@@ -1,1 +1,18 @@
 test -x ~/.local/bin/q; and eval (~/.local/bin/q init fish post --rcfile 99_fig_post | string split0)
+function fish_prompt
+    # Capture the last command's exit status
+    set -l last_status $status
+
+    # Set color based on status
+    if test $last_status -eq 0
+        set_color green # Success (command exists and ran fine)
+    else
+        set_color red # Failure (command not found or errored)
+    end
+
+    # Print a simple prompt (e.g., "> ")
+    echo -n "> "
+
+    # Reset color to normal
+    set_color normal
+end
